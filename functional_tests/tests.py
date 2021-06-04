e4239428d3a352ceba1a87e1149067d318098ab2
MingYang-Yin <1959866131@qq.com>
2021-06-04 17:26:35 +0800

    Use Bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,68 +1,65 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import WebDriverException
 import time
 import unittest
+from selenium.common.exceptions import WebDriverException
 
-MAX_WAIT = 10
+MAX_WAIT=10
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
+
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 
 	def tearDown(self):
+		# self.browser.refresh()
 		self.browser.quit()
 
 	def test_multiple_users_can_start_lists_at_different_urls(self):
 		self.browser.get(self.live_server_url)
-		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox=self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Buy peacock feathers')
 		inputbox.send_keys(Keys.ENTER)
 		self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-		edith_list_url = self.browser.current_url
-		self.assertRegex(edith_list_url, '/lists/.+')
+		edith_list_url=self.browser.current_url
+		self.assertRegex(edith_list_url,'/lists/.+')
 
 		self.browser.quit()
-		self.browser = webdriver.Firefox()
+		self.browser=webdriver.Firefox()
 
 		self.browser.get(self.live_server_url)
-		page_text = self.browser.find_element_by_tag_name('body').text
+		page_text=self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers',page_text)
-		self.assertNotIn('make a fly', page_text)
+		self.assertNotIn('make a fly',page_text)
 
-		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox=self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Buy milk')
 		inputbox.send_keys(Keys.ENTER)
 		self.wait_for_row_in_list_table('1: Buy milk')
 
-		francis_list_url = self.browser.current_url
-		self.assertRegex(francis_list_url, '/lists/.+')
-		self.assertNotEqual(francis_list_url, edith_list_url)
+		francis_list_url=self.browser.current_url
+		self.assertRegex(francis_list_url,'/lists/.+')
+		self.assertNotEqual(francis_list_url,edith_list_url)
 
-		page_text = self.browser.find_element_by_tag_name('body').text
+		page_text=self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers',page_text)
-		self.assertIn('Buy milk', page_text)
+		self.assertIn('Buy milk',page_text)
+
 
 	def wait_for_row_in_list_table(self, row_text):
-		start_time = time.time()
+		start_time=time.time()
 		while True:
 			try:
-				table = self.browser.find_element_by_id('id_list_table')
-				rows = table.find_elements_by_tag_name('tr')
+				table=self.browser.find_element_by_id('id_list_table')
+				rows=table.find_elements_by_tag_name('tr')
 				self.assertIn(row_text, [row.text for row in rows])
 				return
-			except (AssertionError, WebDriverException) as e:
-				if time.time() - start_time > MAX_WAIT:
+			except(AssertionError, WebDriverException) as e:
+				if time.time()-start_time > MAX_WAIT:
 					raise e
 				time.sleep(0.5)
-
-
-	#def check_for_row_in_list_table(self, row_text):
-		#table=self.browser.find_element_by_id('id_list_table')
-		#rows=table.find_elements_by_tag_name('tr')
-		#self.assertIn(row_text, [row.text for row in rows])
 
 	def test_can_start_a_list_for_one_user(self):
 		# Edith has heard about a cool new online to-do app. She goes
@@ -90,33 +87,17 @@ class NewVisitorTest(LiveServerTestCase):
         # when she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do lists
 		inputbox.send_keys(Keys.ENTER)
-		#time.sleep(1)
 		self.wait_for_row_in_list_table('1: Buy peacock feathers')
+		#1  time.sleep(1)
 		#self.check_for_row_in_list_table('1:Buy peacock feathers')
 
-		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox=self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
-		#time.sleep(1)
-
-		# rows=table.find_elements_by_tag_name('tr')
-		# self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
-		# self.assertIn(
-		# 	'2:Use peacock feathers to make a fly',
-		# 	[row.text for row in rows]
-		# 	)
-		# self.assertTrue(
-		# 	any(row.text=='1:Buy peacock feathers' for row in rows),
-		# 	f"New to-do item did not appear in table.Contents were\n{table.text}"
-		# 	)
+		
 		self.wait_for_row_in_list_table('1: Buy peacock feathers')
 		self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
-		#self.check_for_row_in_list_table('1: Buy peacock feathers')
-		#self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-        # There is still a text box inviting her to add another item. She
-        # enters "Use peacock feathers to make a fly"(Edith is very methodical)
-		#self.fail('Finish the test!')
 
 	def test_layout_and_styling(self):
 		# Edith goes to the home page
@@ -143,14 +124,10 @@ class NewVisitorTest(LiveServerTestCase):
 			delta=10
 			)
 
-# The page updates again, and now shows both items on her list
 
-# Edith wonders whether the site will remember her list. The she sees
-# that the site has generated a unique URL for her -- there is some
-# explanatory text to that effect.
 
-# She visits that URL - her to-do list is still there.
 
+      
 # Satisfied, she goes back to sleep
 if __name__ == '__main__':
 	unittest.main(warnings = 'ignore')
